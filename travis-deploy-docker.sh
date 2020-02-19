ceff981aa9b120f5308663ffac86ac501e4baa9f
Philipp Hertweck <philipp.hertweck@iosb.fraunhofer.de>
2020-02-19 16:39:15 +0100

    [Travis]: Directly push

--- travis-deploy-docker.sh
+++ travis-deploy-docker.sh
@@ -5,9 +5,9 @@ mvn install -DskipTests -Dmaven.javadoc.skip=true
 
 printf "Start building images"
 ls
-cd FROST-Server.HTTP && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-http:latest . && cd ..
-cd FROST-Server.MQTT && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-mqtt:latest . && cd ..
-cd FROST-Server.MQTTP && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server:latest . && cd ..
+cd FROST-Server.HTTP && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-http:$TAG --push . && cd ..
+cd FROST-Server.MQTT && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-mqtt:$TAG --push . && cd ..
+cd FROST-Server.MQTTP && docker buildx build --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server:$TAG --push . && cd ..
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   docker push fraunhoferiosb/frost-server-http:latest
@@ -16,10 +16,10 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
 fi
 echo "TAG:"
 echo $TAG
-docker tag fraunhoferiosb/frost-server-http:latest fraunhoferiosb/frost-server-http:$TAG
-docker tag fraunhoferiosb/frost-server-mqtt:latest fraunhoferiosb/frost-server-mqtt:$TAG
-docker tag fraunhoferiosb/frost-server:latest fraunhoferiosb/frost-server:$TAG
+#docker tag fraunhoferiosb/frost-server-http:latest fraunhoferiosb/frost-server-http:$TAG
+#docker tag fraunhoferiosb/frost-server-mqtt:latest fraunhoferiosb/frost-server-mqtt:$TAG
+#docker tag fraunhoferiosb/frost-server:latest fraunhoferiosb/frost-server:$TAG
 
-docker push fraunhoferiosb/frost-server-http:$TAG
-docker push fraunhoferiosb/frost-server-mqtt:$TAG
-docker push fraunhoferiosb/frost-server:$TAG
+#docker push fraunhoferiosb/frost-server-http:$TAG
+#docker push fraunhoferiosb/frost-server-mqtt:$TAG
+#docker push fraunhoferiosb/frost-server:$TAG
