d3bf7f64fff73cb04f49bfcd509775846b79122c
Philipp Hertweck <philipp.hertweck@iosb.fraunhofer.de>
2020-02-20 15:16:21 +0100

    [Travis]: Add more logs for build artifacts

--- travis-deploy-docker-multi-arch.sh
+++ travis-deploy-docker-multi-arch.sh
@@ -8,9 +8,27 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "Start building images"
 echo "Using Tag: "
 echo $TAG
-cd FROST-Server.HTTP && docker buildx build --build-arg WAR_FILE=FROST-Server.HTTP-${version}.war --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-http:$TAG --push . && cd ..
-cd FROST-Server.MQTT && docker buildx build --build-arg JAR_FILE=FROST-Server.MQTT-${version}-jar-with-dependencies.jar --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-mqtt:$TAG --push . && cd ..
-cd FROST-Server.MQTTP && docker buildx build --build-arg WAR_FILE=FROST-Server.MQTTP-${version}.war --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server:$TAG --push . && cd ..
+
+cd FROST-Server.HTTP
+export artifact=FROST-Server.HTTP-${version}.war
+echo "artifact: "
+echo $artifact
+docker buildx build --build-arg WAR_FILE=$artifact --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-http:$TAG --push .
+cd ..
+
+cd FROST-Server.MQTT
+export artifact=FROST-Server.MQTT-${version}-jar-with-dependencies.jar
+echo "artifact: "
+echo $artifact
+docker buildx build --build-arg JAR_FILE=$artifact --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server-mqtt:$TAG --push .
+cd ..
+
+cd FROST-Server.MQTTP
+export artifact=FROST-Server.MQTTP-${version}.war
+echo "artifact: "
+echo $artifact
+docker buildx build --build-arg WAR_FILE=$artifact --platform amd64,linux/arm64/v8,linux/arm/v7 --tag fraunhoferiosb/frost-server:$TAG --push .
+cd ..
 
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
 
